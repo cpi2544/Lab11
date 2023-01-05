180c9009bf03fddcfcad7480c9b9a57de0ebfdcf
Your NameCpi2544 <pokpong1789@gmail.com>
2023-01-05 18:48:41 +0700

    lab11.cpp completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -1,18 +1,25 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
-int main (){
+int main () {
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+    string textline;
+    dest << "-------------------- BOOM ---------------------" << endl;
+    while(getline(source,textline)) {
+		dest << textline << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
+    
     source.close();
     dest.close();
+
 	return 0;
 }
--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ifstream source;
+    source.open("score.txt");
+
+    string textline;
+    double sum = 0;
+    double count = 0;
+    double mean = 0;
+    double sd = 0;
+    double sum2 = 0;
+    while(getline(source,textline)) {
+        sum += stof(textline);
+        sum2 += pow(stof(textline),2);
+        count++;
+    }
+    mean = sum/count;
+    sd = sqrt((sum2/count)-pow(mean,2));
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd << endl;
+
+    source.close();
+    
+    return 0;
+}
